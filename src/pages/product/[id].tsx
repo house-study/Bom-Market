bf4c840234b07bdebde85ef0cb158292a8c0e9f9
Saebom <spring180@naver.com>
2025-04-22 19:14:29 +0900

    chore: 개발 환경 수정 (서버 동시 실행)

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,6 @@
+import axios from 'axios';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import axios from 'axios';
 import { useState, useEffect } from 'react';
 
 const URL = 'http://localhost:3001';
