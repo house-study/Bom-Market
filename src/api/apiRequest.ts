c27eaf3f41e7bd3854300642db5320cb970378b0
Saebom <spring180@naver.com>
2025-04-29 16:41:00 +0900

    fix: 오타 수정 & 타입 지정 수정

--- src/api/apiRequest.ts
+++ src/api/apiRequest.ts
@@ -1,12 +1,12 @@
 import axiosInstance from '@/api/axiosInstance';
 import { ProductType } from '@/types/product';
 
-export const getProducts = async (): Promise<ProductType[]> => {
-  const res = await axiosInstance.get<ProductType[]>('/products/ㅎ');
+export const getProducts = async () => {
+  const res = await axiosInstance.get<ProductType[]>('/products');
   return res.data;
 };
 
-export const getProductDetail = async (id: number): Promise<ProductType> => {
+export const getProductDetail = async (id: number) => {
   const res = await axiosInstance.get<ProductType>(`/products/${id}`);
   return res.data;
 };
